0fb3338af8e8339694b8714c6347e12a4cdcc97b
kamaya <amayaosorio@google.com>
2023-01-09 03:21:11 -0500

    Select Screen added

--- views/MainGame.js
+++ views/MainGame.js
@@ -4,7 +4,7 @@ function MainGame({navigation}) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Main Game</Text>
-    </View>
+     </View>
   );
 }
 
